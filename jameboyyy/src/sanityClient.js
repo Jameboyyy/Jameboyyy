1a6caf84b09788595fd2d05cfcb6227d79121436
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2024-07-08 17:42:34 -0700

    Sanity

--- jameboyyy/src/sanityClient.js
+++ jameboyyy/src/sanityClient.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: 'rpjre3kd',
-    dataset: 'production',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: process.env.REACT_APP_SANITY_DATASET,
     apiVersion: '2022-03-07',
     token: 'skiuyo1aswJINzuhTC2ZLLXl4NX5jNJbOKjPQABzDC5lYzx9EmX0gqkXMGyerz8Cj4ygXLNodUtnfWVxYgzJqyGzmNpgthqIAvWHS8hnJWmFrTqBJ6MTgN1vTxwWR7FK4TN4Vqq1cQQstAMkTou02CobmsMV2mjngShWRMIKH37CCNbco8Z2',
     useCdn: true,
