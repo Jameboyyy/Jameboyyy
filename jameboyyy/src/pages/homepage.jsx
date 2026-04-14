4184e7bb8d9b84e5655263894dcad80465b60fce
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2026-04-14 09:59:06 -0700

    Removed blogs import

--- jameboyyy/src/pages/homepage.jsx
+++ jameboyyy/src/pages/homepage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Landscape from '../sections/landscape';
 import Projects from '../sections/projects';
-import Blogs from '../sections/blogs';
 import Contact from '../sections/contact';
 import Footer from '../components/footer';
 
@@ -13,9 +12,6 @@ const Homepage = () => {
       </section>
       <section id="projects">
         <Projects />
-      </section>
-      <section id="blogs">
-        <Blogs />
       </section>
       <section id="contact">
         <Contact />
